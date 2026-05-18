4441f46e6c780abfdcc3d31ce28150ddb606e0cf
Phil Hoyt <philhoyt@users.noreply.github.com>
2026-05-17 20:06:07 -0400

    fix: add media-gallery view.js to webpack entry map so it gets compiled

---
+++ build/blocks/media-gallery/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '1db370f9dd7367f02d8d');
--- webpack.config.js
+++ webpack.config.js
@@ -47,10 +47,11 @@ module.exports = {
 
 		return {
 			...discovered,
-			'blocks/composer/view':    path.resolve( __dirname, 'src/blocks/composer/view.js' ),
-			'blocks/delete-post/view':  path.resolve( __dirname, 'src/blocks/delete-post/view.js' ),
-			'blocks/edit-post/view':    path.resolve( __dirname, 'src/blocks/edit-post/view.js' ),
-			'blocks/share-post/view':   path.resolve( __dirname, 'src/blocks/share-post/view.js' ),
+			'blocks/composer/view':       path.resolve( __dirname, 'src/blocks/composer/view.js' ),
+			'blocks/delete-post/view':    path.resolve( __dirname, 'src/blocks/delete-post/view.js' ),
+			'blocks/edit-post/view':      path.resolve( __dirname, 'src/blocks/edit-post/view.js' ),
+			'blocks/share-post/view':     path.resolve( __dirname, 'src/blocks/share-post/view.js' ),
+			'blocks/media-gallery/view':  path.resolve( __dirname, 'src/blocks/media-gallery/view.js' ),
 		};
 	},
 };
